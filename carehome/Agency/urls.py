b7677b3fcd662f3b66dd00eaf9941f5528e1a67e
Gnanadeepam Subbiah <Gnanadeepam2.subbiah@live.uwe.ac.uk>
2024-06-15 11:52:05 +0100

    Login pages added

---
+++ carehome/Agency/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # agency
+    path("agencyloginpage/",views.agencylogin,name='login-page')
+
+
+
+
+
+
+
+    # staff
+
+
+
+
+
+
+    # client
+
+
+
+
+]
